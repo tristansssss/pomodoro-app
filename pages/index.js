a5ab33e832cba202bc264119c7e80714453c9fb3
ts <tristansetha@gmail.com>
2021-10-01 13:19:03 -0700

    jsconfig.json alias

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from '@components/layout/layout';
 
 const Index = () => {
   return (
-    <div>
+    <Layout>
       Home
-    </div>
+    </Layout>
   );
 }
 
