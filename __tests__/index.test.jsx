21416b403cdaba54033316ce2b73de76e0adcf80
ts <tristansetha@gmail.com>
2021-10-01 15:36:09 -0700

    configure jest

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,21 @@
+// __tests__/index.test.jsx
+
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+describe.skip('Home', () => {
+  it('Contains Head', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next\.js!/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom/extend-expect'
+
+
+// If you need to add more setup options before each test, it's common to add them here
