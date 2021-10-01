8ff335f69d040c8bfeb572de68344d4d281b4562
ts <tristansetha@gmail.com>
2021-10-01 14:28:53 -0700

    npm install -D identity-obj-proxy react-test-renderer

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,13 +1,28 @@
 import styled from "@emotion/styled"
+import Head from "next/head"
 
 const LayoutContainer = styled.div`
-
+  border: 1px solid black;
 `;
 
 export default function Layout({ children }) {
   return (
-    <LayoutContainer>
-      {children}
-    </LayoutContainer>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <title>Pomodoro App</title>
+      </Head>
+      <LayoutContainer>
+        {children}
+      </LayoutContainer>
+    </>
+
   )
 }
